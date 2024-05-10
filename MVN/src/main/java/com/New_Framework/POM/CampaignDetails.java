e9775fd553ee79c19324bf648aac50539b879dc2
rushm <rushm@LAPTOP-DENRBFPO>
2024-05-10 15:50:51 +0530

    First Commit

---
+++ MVN/src/main/java/com/New_Framework/POM/CampaignDetails.java
@@ -0,0 +1,23 @@
+package com.New_Framework.POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CampaignDetails {
+	@FindBy (id="value_Campaign Name")
+	private WebElement CampaignNameValue;
+	
+	public CampaignDetails(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+	public String getCampaignNameValue()
+	{
+		
+		return CampaignNameValue.getText();
+	}
+
+}
